338ecc6d192e680c7c4b9b752531b95d04e541d9
Somil Kwatra <Somilkwatra21@gmail.com>
2025-10-03 22:22:26 +0530

    feat: setup app navigation.

--- src/Navigator/AppNavigator.tsx
+++ src/Navigator/AppNavigator.tsx
@@ -1,14 +1,26 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import Login from '../Screens/Login/Login';
+import Posts from '../Screens/AllPosts/AllPosts';
+import PostDetails from '../Screens/Posts/Posts';
 
-function AppNavigator() {
+export type RootStackParamList = {
+  Login: undefined;
+  Posts: undefined;
+  PostDetails: { postId: number };
+};
+
+const {Navigator,Screen} = createNativeStackNavigator<RootStackParamList>();
+
+export default function AppNavigator() {
   return (
-<View>
-    <Text>
-        App Navigator
-    </Text>
-</View>
-)
+    <NavigationContainer>
+      <Navigator initialRouteName="Login">
+        <Screen name="Login" component={Login} options={{ title: 'Login' }} />
+        <Screen name="Posts" component={Posts} options={{ title: 'All Posts' }} />
+        <Screen name="PostDetails" component={PostDetails} options={{ title: 'Post Details' }} />
+      </Navigator>
+    </NavigationContainer>
+  );
 }
-
-export default AppNavigator
